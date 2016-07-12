c16a56f939266c14218755a507ae7fe5c8fc30d5
Julio Barrios <juliobarmi@gmail.com>
2016-07-12 10:04:26 -0300

    Adds more data in the scenario.

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -5,4 +5,14 @@ export default function(server) {
   });
 
   server.createList('post', 5, { author });
+
+  server.createList('author', 5);
+
+  server.createList('post', 3, {
+    authorId: 2
+  });
+
+  server.createList('post', 6, {
+    authorId: 3
+  });
 }
